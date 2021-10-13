f3ab31b50c5f5c3191002c92ebae644bbee2ca44
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-10-13 16:13:47 +0600

    signIn & signOut using firebase auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import 'firebase/auth';
+import * as firebase from 'firebase/app';
+import firebaseConfig from './firebase.config';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import {useState} from "react";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+    const [user, setUser] = useState({
+        isSignedIn: false,
+        name: '',
+        email: '',
+        photo: ''
+    });
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+  const handleSignIn = () => {
+    signInWithPopup(auth,provider)
+        .then(res => {
+            const {displayName, photoURL, email} = res.user;
+            const signedInUser = {
+                isSignedIn: true,
+                name: displayName,
+                email: email,
+                photo: photoURL
+            }
+            setUser(signedInUser);
+          console.log(displayName,email,photoURL);
+        })
+        .catch(err => {
+            console.log(err);
+            console.log(err.message);
+        })
+  }
+
+  const handleSignOut = () => {
+      signOut(auth).then( res => {
+          const signOutUser = {
+              isSignedIn: false,
+              name: '',
+              email: '',
+              photo: ''
+          }
+          setUser(signOutUser);
+      }).catch((error) => {
+          // An error happened.
+      });
+      console.log("sign out");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        {
+            user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button>
+                :
+                <button onClick={handleSignIn}>Sign In</button>
+        }
+        {
+            user.isSignedIn && <div>
+                <p>Welcome, {user.name}</p>
+                <p>Your Email: {user.email}</p>
+                <img src={user.photo} alt=""/>
+            </div>
+        }
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAapfh1lajcuALnYGNWDejlce16h1RFrw",
+    authDomain: "amzon-simple-version.firebaseapp.com",
+    projectId: "amzon-simple-version",
+    storageBucket: "amzon-simple-version.appspot.com",
+    messagingSenderId: "384589884984",
+    appId: "1:384589884984:web:ecdbb16accea4a947000cd"
+  };
+
+  export default firebaseConfig;
