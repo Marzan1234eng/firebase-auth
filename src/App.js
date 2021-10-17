8c3dc7956f2ba6a5bad8c090db5b0778f48d43ff
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-10-17 18:12:44 +0600

    firebase user created from submit form & duplicate gmail show error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as firebase from 'firebase/app';
 import firebaseConfig from './firebase.config';
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import {useState} from "react";
+import {createUserWithEmailAndPassword} from "firebase/auth";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -13,10 +14,13 @@ function App() {
         isSignedIn: false,
         name: '',
         email: '',
-        photo: ''
+        password: '',
+        photo: '',
+        error: ''
     });
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
+
 
   const handleSignIn = () => {
     signInWithPopup(auth,provider)
@@ -26,7 +30,8 @@ function App() {
                 isSignedIn: true,
                 name: displayName,
                 email: email,
-                photo: photoURL
+                photo: photoURL,
+                error: ''
             }
             setUser(signedInUser);
           console.log(displayName,email,photoURL);
@@ -43,7 +48,9 @@ function App() {
               isSignedIn: false,
               name: '',
               email: '',
-              photo: ''
+              photo: '',
+              error: '',
+              success: false
           }
           setUser(signOutUser);
       }).catch((error) => {
@@ -52,6 +59,50 @@ function App() {
       console.log("sign out");
   }
 
+  const handleSubmit = (e) => {
+      console.log(user.email, user.password);
+      if (user.email && user.password){
+          createUserWithEmailAndPassword(auth, user.email, user.password)
+              .then((userCredential) => {
+                  // Signed in
+                  const user = userCredential.user;
+                  // ...
+              })
+              .then(res=>{
+                  const newUserInfo = {...user};
+                  newUserInfo.error = '';
+                  newUserInfo.success = true;
+                  setUser(newUserInfo);
+                  //console.log(res);
+              })
+              .catch((error) => {
+                  const newUserInfo = {...user};
+                  newUserInfo.error = error.message;
+                  newUserInfo.success = false;
+                  setUser(newUserInfo);
+              });
+      }
+      e.preventDefault(); // puro page load howa bondho korbe
+  }
+
+    const handleBlur = (e) => {
+        let isFieldValid = 'true';
+        if (e.target.name === 'email'){
+            isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+        }
+        if (e.target.name === 'password'){
+            const isPasswordValid = e.target.value.length > 6;
+            const isPasswordHasNumber = /\d{1}/.test(e.target.value);
+            isFieldValid = isPasswordHasNumber && isPasswordValid;
+        }
+
+        if (isFieldValid){
+            const newUserInfo = {...user};
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
+        }
+    }
+    //console.log(newUser);
   return (
     <div className="App">
 
@@ -64,11 +115,25 @@ function App() {
             user.isSignedIn && <div>
                 <p>Welcome, {user.name}</p>
                 <p>Your Email: {user.email}</p>
+                <p>Password: {user.password}</p>
                 <img src={user.photo} alt=""/>
             </div>
         }
+        <h1>Our own Authentication</h1>
+
+        <form onSubmit={handleSubmit} action="">
+            <input placeholder="Name" name="name" onBlur={handleBlur} type="text" required/>
+            <br/>
+            <input placeholder="Email" name="email" onBlur={handleBlur} type="text" required/>
+            <br/>
+            <input placeholder="Password" name="password" onBlur={handleBlur} type="password" required/>
+            <br/>
+            <input type="submit" value="submit"/>
+        </form>
+        <p style={{color:"red"}}>{user.error}</p>
+        { user.success && <p style={{color: "green"}}>User created successfully</p>}
     </div>
   );
-};
+}
 
 export default App;
